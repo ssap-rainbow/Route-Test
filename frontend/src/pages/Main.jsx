08b57cbda6378064d799c6cbf69d6d00fde9d52f
장정안 <wjddks96@naver.com>
2023-11-14 16:22:53 +0900

    feat: 링크로 test페이지로 이동하게 변경

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import Test from "./Test";
+import { Link } from "react-router-dom";
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      Main페이지
+      <Link to="/test">Test페이지로 이동</Link>
+    </div>
+  );
 }
